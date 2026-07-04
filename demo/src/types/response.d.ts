9fe41b09d02496b34afe26632a70da286892b952
devAhmad <ahmadhussain554@gmail.com>
2026-07-04 10:34:07 +0000

    update

--- demo/src/types/response.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '../utils/response' {
-  export class ApiResponse {
-    static success(res: any, message: string, data?: any): any;
-    static error(res: any, message: string, status?: number): any;
-  }
-}
